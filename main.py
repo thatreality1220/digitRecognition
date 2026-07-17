7902610896c0701d56dd7dc8eba3177c7d217f88
Daniel Lee <joony1220@gmail.com>
2026-07-16 18:03:14 -0700

    Wrote descriptions in the code to better explain the program.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 root = Tk()
 root.title("Digit Classifier!")
 root.geometry("600x600")
-canvas = Canvas(root, bg="white", highlightthickness=0)
+canvas = Canvas(root, bg="white", highlightthickness=0) # configure our tkinter interface
 pen_color = "black"
 brush_size = 12;
 last_x = None
@@ -17,17 +17,18 @@ pred_label = tk.Label(root, text="")
 pred_label.pack(side="bottom", anchor="se")
 
 conf_label = tk.Label(root, text="")
-conf_label.pack(side="bottom", anchor="se")
+conf_label.pack(side="bottom", anchor="se") #adds our prediction and confidence labels to update later
 def draw(event):
     global last_x, last_y
     if last_x and last_y:
         canvas.create_line(last_x, last_y, event.x, event.y, fill=pen_color, width=brush_size, capstyle=tk.ROUND, smooth=tk.TRUE)
+    # draw a line using our current and previous positions if the values of last_x and last_y are not None
     last_x = event.x
-    last_y = event.y
-
+    last_y = event.y 
+    # set our previous values to be equal to the current values to keep drawing a line
 def start_draw(event):
     global last_x, last_y
-    last_x = event.x
+    last_x = event.x # sets our last mouse positions to the current ones since its the first position
     last_y = event.y
 
 def stop_draw(event):
@@ -36,34 +37,35 @@ def stop_draw(event):
     last_y = None
 
 def erase_all():
-    canvas.delete("all")
+    canvas.delete("all") #deletes everything drawn on the canvas
 def model_result():
     x1 = canvas.winfo_rootx()
     y1 = canvas.winfo_rooty()
     x2 = x1 + canvas.winfo_width()
     y2 = y1 + canvas.winfo_height()
+    # calculate the bounding box dimensions for our tkinter screen
+    screenshot = ImageGrab.grab(bbox=(x1, y1, x2, y2)) #saves a copy of an image of the area inside our box
+    screenshot = (screenshot.convert('L')).resize((28, 28)) # converts the image into grayscale and resizes it into a 28x28 pixel image
+    screenshot = ImageOps.invert(screenshot) #inverts the colors so that the drawing is white and the background is black (to make the screenshot similar to our model's training data)
 
-    screenshot = ImageGrab.grab(bbox=(x1, y1, x2, y2))
-    screenshot = (screenshot.convert('L')).resize((28, 28))
-    screenshot = ImageOps.invert(screenshot)
 
-    prediction, confidence = predict(screenshot)
+    prediction, confidence = predict(screenshot) # call our predict function
 
-    pred_label.config(text=f"Prediction: {prediction}")
+    pred_label.config(text=f"Prediction: {prediction}") #update the labels with our prediction and confidence 
     conf_label.config(text=f"Confidence: {confidence:.2%}")
 
 def predict(data):
     CLASS_LABELS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    model = keras.models.load_model("digit_classifier.keras")
-    data = np.array(data)
+    model = keras.models.load_model("digit_classifier.keras") #load our model from our saved file
+    data = np.array(data) # convert our screenshot into a numpy array
 
     data = (data.astype("float32") / 255.0)[..., np.newaxis]
-    data = np.expand_dims(data, axis=0)
-    predictions = model.predict(data, batch_size=None, verbose=0, steps=None, callbacks=None)
+    data = np.expand_dims(data, axis=0) # fit the image data to give our model
+    predictions = model.predict(data, batch_size=None, verbose=0, steps=None, callbacks=None) # model gives us probabilities for each digit
     
     prediction = np.argmax(predictions, axis=-1)[0]
-    predicted_digit = CLASS_LABELS[prediction]
-    confidence = predictions[0][prediction]
+    predicted_digit = CLASS_LABELS[prediction] #gives us a clear prediction based on our digits
+    confidence = predictions[0][prediction] #gives us the model's calculated probability for our predicted number
 
     return predicted_digit, confidence
 canvas.pack(fill=tk.BOTH, expand=True)
@@ -72,9 +74,10 @@ eraseButton.pack(side="bottom", anchor="sw")
 
 predictButton = tk.Button(root, text="Predict", width=10, command=model_result)
 predictButton.pack(side="bottom", anchor="sw")
-
+#add our predict and erase buttons with their corresponding functions above
 canvas.bind("<Button-1>", start_draw)
 canvas.bind("<B1-Motion>", draw)
-canvas.bind("<ButtonRelease-1>", stop_draw)
+canvas.bind("<ButtonRelease-1>", stop_draw) #applies our functions to each of the inputs
 
 root.mainloop()
+#runs the code indefinitely until the user exits out of the program
--- model.py
+++ model.py
@@ -2,31 +2,33 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-(X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
+(X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data() # load the keras mnist dataset into a test set and training data set
 
 X_train = (X_train / 255.0)[..., np.newaxis]
-X_test = (X_test / 255.0)[..., np.newaxis]
+X_test = (X_test / 255.0)[..., np.newaxis] #normalize our training and testing data + add a new axis to match keras' syntax
 
 model = keras.Sequential([
-    keras.layers.Input(shape=(28, 28, 1)),
+    keras.layers.Input(shape=(28, 28, 1)), #build our input layer that takes in our 28x28 pixel image
 
-    keras.layers.RandomTranslation(0.1, 0.1),
+    keras.layers.RandomTranslation(0.1, 0.1), 
     keras.layers.RandomRotation(0.02),
-    keras.layers.RandomZoom(0.1),
+    keras.layers.RandomZoom(0.1), #randomly alter the image so that it's more realistic
     
-    keras.layers.Conv2D(32, (3, 3), activation="relu"),
-    keras.layers.MaxPooling2D((2, 2)),
+    keras.layers.Conv2D(32, (3, 3), activation="relu"), #places a 3x3 pixel filter throughout our image
+    keras.layers.MaxPooling2D((2, 2)), #take the largest values from our 3x3 filter
 
     keras.layers.Conv2D(64, (3, 3), activation="relu"),
     keras.layers.MaxPooling2D((2, 2)),
 
-    keras.layers.Flatten(),
-    keras.layers.Dense(64, activation="relu"),
-    keras.layers.Dense(10, activation="softmax")
+    keras.layers.Flatten(), # change our 28x28 image into a 1D array of numbers (0-1)
+    keras.layers.Dense(64, activation="relu"), # 1 normal hidden layer with 64 neurons
+    keras.layers.Dense(10, activation="softmax") #gives us 10 probabilties for each of the digits
 ])
 
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-history = model.fit(X_train, y_train, validation_split=0.1, epochs=5, verbose=1)
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]) # configues our model's optimizer and our way of calculating loss
+history = model.fit(X_train, y_train, validation_split=0.1, epochs=5, verbose=1) # splits our dataset into 90% training data and 10% testing data
+#and adjusts how many iterations we train our model for
+
 
 model.save("digit_classifier.keras")
-
+# saves our trained model into a seperate .keras file
